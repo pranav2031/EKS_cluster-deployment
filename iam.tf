a799b9204e43b8af2829f827e7566d973c32be76
pranav2031 <131767306+pranav2031@users.noreply.github.com>
2025-10-18 20:43:39 +0530

    Update iam.tf

--- iam.tf
+++ iam.tf
@@ -23,19 +23,7 @@ resource "aws_iam_role_policy_attachment" "AmazonEKSVPCResourceController" {
 }
 
 
-resource "aws_eks_cluster" "project_cluster" {
-  name     = "project-cluster"
-  role_arn = aws_iam_role.eks_role.arn
 
-  vpc_config {
-    subnet_ids = aws_subnet.public[*].id
-  }
-
-  depends_on = [
-    aws_iam_role_policy_attachment.AmazonEKSClusterPolicy,
-    aws_iam_role_policy_attachment.AmazonEKSVPCResourceController
-  ]
-}
 
 
 
